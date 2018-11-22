8bc446550de3e960e43d09a10075ad4ebc1ce667
yinglong2001 <344924464@qq.com>
2018-11-22 10:36:41 +0800

    1

---
+++ Cylong.Snowflake/Snowflake.cs
@@ -0,0 +1,185 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cylong.Snowflake
+{
+
+
+    /**
+ * <br>
+ * SnowFlake的结构如下(每部分用-分开):<br>
+ * 0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000 <br>
+ * 1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0<br>
+ * 41位时间截(毫秒级)，注意，41位时间截不是存储当前时间的时间截，而是存储时间截的差值（当前时间截 - 开始时间截)
+ * 得到的值），这里的的开始时间截，一般是我们的id生成器开始使用的时间，由我们程序来指定的（如下下面程序IdWorker类的startTime属性）。41位的时间截，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69<br>
+ * 10位的数据机器位，可以部署在1024个节点，包括5位datacenterId和5位workerId<br>
+ * 12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号<br>
+ * 加起来刚好64位，为一个Long型。<br>
+ * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高，经测试，SnowFlake每秒能够产生26万ID左右。
+ */
+
+    /// <summary>
+    /// 动态生产有规律的ID
+    /// </summary>
+    public class Snowflake
+    {
+        private static long machineId;//机器ID
+        private static long datacenterId = 0L;//数据ID
+        private static long sequence = 0L;//计数从零开始
+
+        private static long twepoch = 687888001020L; //唯一时间随机量
+
+        private static long machineIdBits = 5L; //机器码字节数
+        private static long datacenterIdBits = 5L;//数据字节数
+        public static long maxMachineId = -1L ^ -1L << (int)machineIdBits; //最大机器ID
+        private static long maxDatacenterId = -1L ^ (-1L << (int)datacenterIdBits);//最大数据ID
+
+        private static long sequenceBits = 12L; //计数器字节数，12个字节用来保存计数码        
+        private static long machineIdShift = sequenceBits; //机器码数据左移位数，就是后面计数器占用的位数
+        private static long datacenterIdShift = sequenceBits + machineIdBits;
+        private static long timestampLeftShift = sequenceBits + machineIdBits + datacenterIdBits; //时间戳左移动位数就是机器码+计数器总字节数+数据字节数
+        public static long sequenceMask = -1L ^ -1L << (int)sequenceBits; //一微秒内可以产生计数，如果达到该值则等到下一微妙在进行生成
+        private static long lastTimestamp = -1L;//最后时间戳
+
+        private static object syncRoot = new object();//加锁对象
+        static Snowflake snowflake;
+
+        public static Snowflake Instance()
+        {
+            if (snowflake == null)
+                snowflake = new Snowflake();
+            return snowflake;
+        }
+
+        public Snowflake()
+        {
+            Snowflakes(0L, -1);
+        }
+
+        public Snowflake(long machineId)
+        {
+            Snowflakes(machineId, -1);
+        }
+
+        public Snowflake(long machineId, long datacenterId)
+        {
+            Snowflakes(machineId, datacenterId);
+        }
+
+        private void Snowflakes(long machineId, long datacenterId)
+        {
+            if (machineId >= 0)
+            {
+                if (machineId > maxMachineId)
+                {
+                    throw new Exception("机器码ID非法");
+                }
+                Snowflake.machineId = machineId;
+            }
+            if (datacenterId >= 0)
+            {
+                if (datacenterId > maxDatacenterId)
+                {
+                    throw new Exception("数据中心ID非法");
+                }
+                Snowflake.datacenterId = datacenterId;
+            }
+        }
+
+        /// <summary>
+        /// 生成当前时间戳
+        /// </summary>
+        /// <returns>毫秒</returns>
+        private static long GetTimestamp()
+        {
+            return (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+        }
+
+        /// <summary>
+        /// 获取下一微秒时间戳
+        /// </summary>
+        /// <param name="lastTimestamp"></param>
+        /// <returns></returns>
+        private static long GetNextTimestamp(long lastTimestamp)
+        {
+            long timestamp = GetTimestamp();
+            if (timestamp <= lastTimestamp)
+            {
+                timestamp = GetTimestamp();
+            }
+            return timestamp;
+        }
+
+        /// <summary>
+        /// 获取长整形的ID
+        /// </summary>
+        /// <returns></returns>
+        public long GetId()
+        {
+            lock (syncRoot)
+            {
+                long timestamp = GetTimestamp();
+                if (Snowflake.lastTimestamp == timestamp)
+                { //同一微妙中生成ID
+                    sequence = (sequence + 1) & sequenceMask; //用&运算计算该微秒内产生的计数是否已经到达上限
+                    if (sequence == 0)
+                    {
+                        //一微妙内产生的ID计数已达上限，等待下一微妙
+                        timestamp = GetNextTimestamp(Snowflake.lastTimestamp);
+                    }
+                }
+                else
+                {
+                    //不同微秒生成ID
+                    sequence = 0L;
+                }
+                if (timestamp < lastTimestamp)
+                {
+                    throw new Exception("时间戳比上一次生成ID时时间戳还小，故异常");
+                }
+                Snowflake.lastTimestamp = timestamp; //把当前时间戳保存为最后生成ID的时间戳
+                long Id = ((timestamp - twepoch) << (int)timestampLeftShift)
+                    | (datacenterId << (int)datacenterIdShift)
+                    | (machineId << (int)machineIdShift)
+                    | sequence;
+                return Id;
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// 生成随机数
+    /// </summary>
+    /// <param name="length">长度</param>
+    /// <param name="machineId">机器号</param>
+    /// <returns></returns>
+    public class RandomNo
+    {
+        public static long Random(int? length, int? machineId)
+        {
+            //length 不能超过10位
+            int _length = length ?? 4;
+            int _machineId = machineId ?? 1;
+            byte[] randomNumber = new byte[_length];
+            System.Security.Cryptography.RNGCryptoServiceProvider rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            rng.GetBytes(randomNumber);
+            uint randomResult = 0x0;
+            for (int i = 0; i < _length; i++)
+            {
+                randomResult |= ((uint)randomNumber[i] << ((_length - 1 - i) * 8));
+            }
+            long rand = (int)(randomResult) + _machineId;
+            if (rand < 0)
+            {
+                //过滤掉负数
+                rand = -rand;
+            }
+            return rand;
+        }
+    }
+}
+
